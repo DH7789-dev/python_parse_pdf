bde05e516db8f5c8558cfbc7cdd61e65efd41524
DH7789-dev <dharnaud77@hotmail.fr>
2022-04-29 14:49:23 +0200

    fix

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from parse import parser_pdf
+import sys
 
+from parse import parser_pdf
+doc1 = str(sys.argv[1])
+doc2 = str(sys.argv[2])
+
+# print(doc1)
+# print(doc2)
 if __name__ == '__main__':
-    parser_pdf("cc","cc")
+    parser_pdf(doc1,doc2)
--- parse.py
+++ parse.py
@@ -2,15 +2,11 @@ import sys
 import pdfplumber
 
 
-# doc1 = str(sys.argv[1])
-# doc2 = str(sys.argv[2])
 
-# print(doc1)
-# print(doc2)
 
 def parser_pdf(docEnter, docExit):
-    pdf = pdfplumber.open("23 - Potentiel nation DOC relativité Non start .pdf")
-    with open("cc2.text", "a", encoding='utf-8', errors='surrogateescape') as file:
+    pdf = pdfplumber.open(docEnter)
+    with open(docExit, "a", encoding='utf-8', errors='surrogateescape') as file:
         for i in range(len(pdf.pages)):
             page = pdf.pages[i]
             text = page.extract_text()
